4c4016f46577175059d008289267150fdbc2ddb9
황정현 <hijihilin@gmail.com>
2025-04-30 10:25:14 +0900

    style: 사용되지 않는 import문 제거

--- FE/app/src/main/java/com/a303/helpmet/presentation/feature/helmetcheck/HelmetCheckScreen.kt
+++ FE/app/src/main/java/com/a303/helpmet/presentation/feature/helmetcheck/HelmetCheckScreen.kt
@@ -1,6 +1,5 @@
 package com.a303.helpmet.presentation.feature.helmetcheck
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -9,7 +8,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.a303.helpmet.R
 import com.a303.helpmet.ui.theme.HelpmetTheme
 import org.koin.androidx.compose.koinViewModel
