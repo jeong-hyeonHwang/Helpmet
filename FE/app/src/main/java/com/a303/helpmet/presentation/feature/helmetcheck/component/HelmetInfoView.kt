096ada63269dde0aee1e94dd6090ea1fdeb3592a
nahyewon <n417759@gmail.com>
2025-04-30 16:13:18 +0900

    feat: 헬멧 연결 관련 컴포넌트 개발

--- FE/app/src/main/java/com/a303/helpmet/presentation/feature/helmetcheck/component/HelmetInfoView.kt
+++ FE/app/src/main/java/com/a303/helpmet/presentation/feature/helmetcheck/component/HelmetInfoView.kt
@@ -1,12 +1,17 @@
 package com.a303.helpmet.presentation.feature.helmetcheck.component
 
+import android.util.Log
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
 import androidx.compose.material3.*
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.a303.helpmet.R
 import androidx.compose.ui.unit.dp
+import com.a303.helpmet.presentation.feature.helmetcheck.HelmetCheckScreen
 import com.a303.helpmet.presentation.feature.helmetcheck.HelmetCheckViewModel
 import com.a303.helpmet.ui.theme.HelpmetTheme
 import org.koin.androidx.compose.koinViewModel
@@ -19,35 +24,98 @@ fun HelmetInfoView(
 
     Column(
         modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.Start,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.spacedBy(32.dp)
     ) {
-        Row {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Row {
+                Text(
+                    text = "헬프",
+                    style = HelpmetTheme.typography.appTitle,
+                    color = HelpmetTheme.colors.primary
+                )
+                Text(
+                    text = "멧",
+                    style = HelpmetTheme.typography.appTitle,
+                    color = HelpmetTheme.colors.black1
+                )
+            }
             Text(
-                text = "헬프",
-                style = HelpmetTheme.typography.appTitle,
-                color = HelpmetTheme.colors.primary
-            )
-            Text(
-                text = "멧",
-                style = HelpmetTheme.typography.appTitle,
-                color = HelpmetTheme.colors.black1
+                text = stringResource(R.string.safety_info),
+                style = HelpmetTheme.typography.bodySmall,
+                color = HelpmetTheme.colors.gray2
             )
         }
-        Text(
-            text = stringResource(R.string.safety_info),
-            style = HelpmetTheme.typography.bodySmall,
-            color = HelpmetTheme.colors.gray2
-        )
-    }
 
-    // 두 Column 사이 간격
-    Spacer(modifier = Modifier.height(32.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Surface(
+                shape = RoundedCornerShape(10.dp),
+                shadowElevation = 4.dp,
+                color = HelpmetTheme.colors.white1,
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(24.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.spacedBy(24.dp)
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_util_helmet),
+                        contentDescription = "헬멧",
+                        tint = HelpmetTheme.colors.black1,
+                        modifier = Modifier.size(200.dp),
+                    )
 
-    Column {
-        Text("헬멧 연결 정보")
-        Button(onClick = { viewModel.checkConnection() }) {
-            Text(if (isConnected) "연결됨" else "연결하기")
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentHeight(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        Text(
+                            text = stringResource(if (isConnected) R.string.connect_helmet_success else R.string.connect_helmet_prompt),
+                            style = HelpmetTheme.typography.bodySmall
+                        )
+                        Button(
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = if (isConnected) HelpmetTheme.colors.gray2 else HelpmetTheme.colors.primaryLight,
+                                contentColor = if (isConnected) HelpmetTheme.colors.white1 else HelpmetTheme.colors.black1
+                            ),
+                            modifier = Modifier
+                                .height(30.dp),
+                            shape = RoundedCornerShape(20.dp),
+                            contentPadding = PaddingValues(horizontal = 21.dp, vertical = 9.dp),
+                            onClick = { viewModel.checkConnection() }
+                        ) {
+                            Text(
+                                text = if (isConnected) stringResource(R.string.end_connection) else stringResource(R.string.begin_connection),
+                                style = HelpmetTheme.typography.caption
+                            )
+                        }
+                    }
+
+                }
+            }
+
         }
     }
+
+
+}
+
+@Preview
+@Composable
+fun HelmetInfoViewPreview() {
+    HelmetInfoView(
+        viewModel = HelmetCheckViewModel(),
+    )
 }
