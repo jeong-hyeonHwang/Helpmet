8a02b30e4d8804684f66153321a7d1accd8c04d8
nahyewon <n417759@gmail.com>
2025-05-02 17:57:29 +0900

    feat: 추천 코스 내 기능 설명 컴포넌트 구현

---
+++ FE/app/src/main/java/com/a303/helpmet/presentation/feature/preride/component/CourseInfoBubbleView.kt
@@ -0,0 +1,46 @@
+package com.a303.helpmet.presentation.feature.preride.component
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.a303.helpmet.R
+import com.a303.helpmet.presentation.feature.preride.PreRideScreen
+import com.a303.helpmet.ui.theme.HelpmetTheme
+
+@Composable
+fun CourseInfoBubbleView(
+    modifier: Modifier = Modifier
+){
+    val screenWidth = LocalConfiguration.current.screenWidthDp.dp
+    val cardWidth = screenWidth * 0.85f
+
+    Card(
+        modifier = modifier.width(cardWidth),
+        shape = RoundedCornerShape(20.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = HelpmetTheme.colors.white1.copy(alpha = 0.85f),
+            contentColor = HelpmetTheme.colors.black1)
+    ) {
+        Text(
+            text = stringResource(R.string.nearby_course_info),
+            style = HelpmetTheme.typography.bodyMedium,
+            modifier = Modifier.padding(horizontal = 52.dp, vertical = 12.dp),
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreRidePreview(){
+    PreRideScreen(
+        onStartRide={}
+    )
+}
