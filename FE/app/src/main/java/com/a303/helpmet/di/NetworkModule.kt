50d5fb3e7bb917841ddff1ec02ad39608bc9e871
황정현 <hijihilin@gmail.com>
2025-05-16 17:53:54 +0900

    feat: wi-fi 및 데이터 환경 동시 활성화 로직 구현

--- FE/app/src/main/java/com/a303/helpmet/di/NetworkModule.kt
+++ FE/app/src/main/java/com/a303/helpmet/di/NetworkModule.kt
@@ -1,5 +1,6 @@
 package com.a303.helpmet.di
 
+import android.content.Context
 import com.a303.helpmet.data.network.RetrofitProvider
 import com.a303.helpmet.data.network.interceptor.ErrorInterceptor
 import com.a303.helpmet.data.repository.DeviceRepository
@@ -39,19 +40,32 @@ val networkModule = module {
         ErrorInterceptor()
     }
 
-    single {
+//    single {
+//        RetrofitProvider(
+//            jsonParser = get(),
+//            loggingInterceptor = get(),
+//            errorInterceptor = get()
+//        )
+//    }
+    single { (context: Context) ->
         RetrofitProvider(
+            context = context,
             jsonParser = get(),
             loggingInterceptor = get(),
             errorInterceptor = get()
         )
     }
 
+
     // NavigationService를 RetrofitProvider에서 꺼내 등록
     single<NavigationService> { get<RetrofitProvider>().navigationService }
     single { get<RetrofitProvider>().deviceService }
 
     single { DeviceRepository(get()) }
+
+
+    single { GetCellularNetworkUseCase(get()) } // context 주입 필요
+
 }
 
 val fakeNetworkModule = module {
