e2e16688a0414b88dfc5eccaee8af6b8476a9c0e
nahyewon <n417759@gmail.com>
2025-05-19 16:52:10 +0900

    remove: 안쓰는 UseCase 삭제

--- FE/app/src/main/java/com/a303/helpmet/di/VoiceInteractionModule.kt
+++ FE/app/src/main/java/com/a303/helpmet/di/VoiceInteractionModule.kt
@@ -1,10 +1,7 @@
 package com.a303.helpmet.di
 
 import com.a303.helpmet.domain.usecase.SendDirectionCommandUseCase
-import com.a303.helpmet.presentation.feature.navigation.viewmodel.RouteViewModel
 import com.a303.helpmet.presentation.feature.voiceinteraction.VoiceInteractViewModel
-import com.a303.helpmet.presentation.feature.voiceinteraction.usecase.EndGuideUseCase
-import com.a303.helpmet.presentation.feature.voiceinteraction.usecase.NavigateToRentalStationUseCase
 import com.a303.helpmet.presentation.feature.voiceinteraction.usecase.NavigateToPlaceUseCase
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
@@ -14,13 +11,11 @@ val voiceInteractionModule = module {
         VoiceInteractViewModel(
             application = get(),
             navigateToPlace = get(),
-            endGuide = get(),
             sendDirectionCommandUseCase = get(),
         )
     }
 
     factory { NavigateToPlaceUseCase(get()) }
-    factory { EndGuideUseCase() }
     single { SendDirectionCommandUseCase(get()) }
 
 }
