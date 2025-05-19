37c712b62564d7fc810bc53db7858e9e4ab97039
이주은 <wndms058@gmail.com>
2025-05-19 20:26:36 +0900

    fix: 트랜잭션 적용

--- BE/core/database.py
+++ BE/core/database.py
@@ -12,6 +12,5 @@ engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 async def get_db():
-    #transation 설정 제거 (readonly session)
-    session = session.execution_options(isolation_level="AUTOCOMMIT")
-    yield session
+    async with AsyncSessionLocal() as session:
+        yield session
