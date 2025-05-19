4483be4578bfe6b9e11c4770cd73d22c540b488b
nahyewon <n417759@gmail.com>
2025-05-19 16:53:03 +0900

    refactor: 로딩 컴포넌트에서 배경 없앤 버전 추가

--- FE/app/src/main/java/com/a303/helpmet/presentation/feature/preride/component/RoutePreviewLoadingView.kt
+++ FE/app/src/main/java/com/a303/helpmet/presentation/feature/preride/component/RoutePreviewLoadingView.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.a303.helpmet.R
@@ -14,12 +15,16 @@ import com.a303.helpmet.ui.theme.HelpmetTheme
 
 @Composable
 fun RouteMapLoadingView(
-    message: String = stringResource(R.string.dialog_connecting_map)
+    message: String = stringResource(R.string.dialog_connecting_map),
+    isBackgroundBlack: Boolean = true,
 ) {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(HelpmetTheme.colors.black1.copy(alpha = 0.8f)),
+            .background(
+                if (isBackgroundBlack) HelpmetTheme.colors.black1.copy(alpha = 0.8f)
+                else Color.Transparent
+            ),
         contentAlignment = Alignment.Center
     ) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
@@ -32,7 +37,7 @@ fun RouteMapLoadingView(
             Text(
                 text = message,
                 style = HelpmetTheme.typography.bodyMedium,
-                color = HelpmetTheme.colors.white1
+                color = if (isBackgroundBlack) HelpmetTheme.colors.white1 else HelpmetTheme.colors.black1
             )
         }
     }
