5be228ce2868ce2ee5d1a9927fb96f54516a59d0
이주은 <wndms058@gmail.com>
2025-05-19 20:07:05 +0900

    refactor: 읽기 전용 트랜잭션 적용
    - SELECT문만 사용하고 있어서, read only transaction을 적용했습니다.

--- BE/core/database.py
+++ BE/core/database.py
@@ -12,5 +12,6 @@ engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 async def get_db():
-    async with AsyncSessionLocal() as session:
-        yield session
+    #transation 설정 제거 (readonly session)
+    session = session.execution_options(isolation_level="AUTOCOMMIT")
+    yield session
