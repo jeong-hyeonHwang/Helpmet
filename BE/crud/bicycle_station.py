e58666a5f0e7027318ca940f6c7d1e57267442f0
이주은 <wndms058@gmail.com>
2025-05-19 20:05:42 +0900

    refactor: 인덱스 적용

--- BE/crud/bicycle_station.py
+++ BE/crud/bicycle_station.py
@@ -1,8 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
-from geoalchemy2.functions import ST_Distance
-from shapely.geometry import Point
-from geoalchemy2.shape import from_shape
+from sqlalchemy import select, text
 from core.models import BicycleStation
 from typing import List
 
@@ -15,11 +12,11 @@ async def fetch_top_n_bike_stations(
     """
     위경도를 기준으로 가까운 따릉이 대여소 N개를 거리순으로 가져옵니다.
     """
-    user_point = from_shape(Point(lon, lat), srid=4326)
 
     query = (
         select(BicycleStation)
-        .order_by(ST_Distance(BicycleStation.geom, user_point))
+        .order_by(text("geom <-> ST_SetSRID(ST_MakePoint(:lon, :lat), 4326)"))
+        .params(lon=lon, lat=lat)
         .limit(limit)
     )
 
